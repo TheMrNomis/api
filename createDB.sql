9f258192b08dd3c6933334a290d2004bce04874f
Amaury Louarn <amaury.louarn@inria.fr>
2017-12-31 03:24:33 +0100

    removed test data from DB create file

--- createDB.sql
+++ createDB.sql
@@ -1,4 +1,1 @@
 CREATE TABLE IF NOT EXISTS script_data (userID TEXT, dataName TEXT, data TEXT, PRIMARY KEY (userID, dataName));
-
-INSERT INTO script_data VALUES ("n0m1s", "data1", '{foo:"data1"}');
-INSERT INTO script_data VALUES ("n0m1s", "data2", '{foo:"data2"}');
